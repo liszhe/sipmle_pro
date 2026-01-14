a97d6c8a4343a756b01e217fb52b854ecd0af0f2
sveka <gamemrshinshila@outlook.com>
2026-01-14 16:48:54 +0300

    start do project todo list

--- ToDoList/src/main/java/org/example/practiceexample/ToDo.java
+++ ToDoList/src/main/java/org/example/practiceexample/ToDo.java
@@ -1,4 +1,23 @@
 package org.example.practiceexample;
 
+import static org.example.practiceexample.ToDoList.*;
+import static org.example.practiceexample.User.getInput;
+
 public class ToDo {
+    public static void userChoice(String input){
+        while (!input.equals("Выход")){
+            input = getInput();
+            switch (input){
+                case "Добавить":
+                    System.out.println("Напишите задание");
+                    createTask(getInput());
+                case "Проверить":
+                    System.out.println("Напишите задание");
+                    hasTask(getInput());
+                case "Обновить":
+                    System.out.println("Напишите задание");
+                   updateTask(getInput());
+            }
+        }
+    }
 }
--- ToDoList/src/main/java/org/example/practiceexample/ToDoStart.java
+++ ToDoList/src/main/java/org/example/practiceexample/ToDoStart.java
@@ -1,8 +1,15 @@
 package org.example.practiceexample;
 
+import static org.example.practiceexample.ToDo.userChoice;
 import static org.example.practiceexample.User.getInput;
 
 public class ToDoStart {
     public static void main(String[] args) {
+        System.out.println("Выберите действие:\n" +
+                "1) Добавить\n" +
+                "2) Проверить\n" +
+                "3) Обновить\n" +
+                "4) Выход");
+        userChoice(getInput());
     }
 }
